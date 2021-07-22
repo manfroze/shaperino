6f46b0322a3ff1d10d7cf91b76b506342cd65a44
Manfredi Pumo <manfroze@gmail.com>
2021-07-22 13:43:55 +0100

    parallax

--- js/animate.js
+++ js/animate.js
@@ -2,8 +2,17 @@ $( document ).on( "click", "#shaperino", _.throttle(animate, 200) );
 
 function animate(){
 
-	$("#shaperino").animate( {scale: 0.80}, 100);
-	setTimeout(() => { $("#shaperino").animate( {scale: 1}, 100); }, 0);
+	$("#shaperino #main-canvas svg").animate( {scale: 0.80}, 100);
+	setTimeout(() => { $("#shaperino #main-canvas svg").animate( {scale: 1}, 100); }, 0);
+
+	$("#shaperino #charge-canvas svg").animate( {scale: 0.60}, 100);
+	setTimeout(() => { $("#shaperino #charge-canvas svg").animate( {scale: 1}, 100); }, 0);
+
+	$("#shaperino #split-canvas svg").animate( {scale: 0.60}, 100);
+	setTimeout(() => { $("#shaperino #split-canvas svg").animate( {scale: 1}, 100); }, 0);
+
+	$("#shaperino #hyper-canvas svg").animate( {scale: 0.70}, 100);
+	setTimeout(() => { $("#shaperino #hyper-canvas svg").animate( {scale: 1}, 100); }, 0);
 
 
 }
